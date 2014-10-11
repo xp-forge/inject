2a20c0420648827fa29634c93dabb92f10bcc61b
Timm Friebe <thekid@thekid.de>
2014-10-11 12:37:55 +0200

    Use short form

--- src/main/php/inject/InstanceProvider.class.php
+++ src/main/php/inject/InstanceProvider.class.php
@@ -4,21 +4,9 @@
 class InstanceProvider extends \lang\Object implements Provider {
   protected $instance= null;
 
-  /**
-   * Creates a new instance with an instance
-   *
-   * @param   var $instance
-   */
-  public function __construct($instance= null) {
-    $this->instance= $instance;
-  }
+  /** @param var */
+  public function __construct($instance= null) { $this->instance= $instance; }
 
-  /**
-   * Gets an instance of a service
-   *
-   * @return  var
-   */
-  public function get() {
-    return $this->instance;
-  }
+  /** @retun var */
+  public function get() { return $this->instance; }
 }
