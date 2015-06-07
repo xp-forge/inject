6c51b0874f7f5190cc6466334519aec0fde65e78
Timm Friebe <thekid@thekid.de>
2015-06-07 17:37:34 +0200

    Add inject.ConfiguredBindings class which reads from a properties file

---
+++ src/main/php/inject/ConfiguredBindings.class.php
@@ -0,0 +1,102 @@
+<?php namespace inject;
+
+use util\PropertyAccess;
+use lang\XPClass;
+
+/**
+ * Bindings from a properties file
+ *
+ * ```ini
+ * scriptlet.Session=com.example.session.FileSystem
+ * scriptlet.Session=com.example.session.MemCache("tcp://localhost:11211")
+ * ```
+ *
+ * @test    xp://inject.unittest.ConfiguredBindingsTest
+ */
+class ConfiguredBindings extends Bindings {
+  private $properties;
+
+  /** @param util.PropertyAccess */
+  public function __construct(PropertyAccess $properties) { $this->properties= $properties; }
+
+  /**
+   * Parse arguments from a string. Supports strings, booleans, null, and numbers.
+   *
+   * @param  string $input
+   * @return var[]
+   */
+  private function argumentsIn($input) {
+    if ('' === $input) return [];
+
+    $arguments= [];
+    foreach (explode(',', $input) as $arg) {
+      if ('"' === $arg{0} || "'" === $arg{0}) {
+        $arguments[]= strtr(substr($arg, 1, -1), ['\\'.$arg{0} => $arg{0}]);
+      } else if (0 === strncasecmp($arg, 'true', 4)) {
+        $arguments[]= true;
+      } else if (0 === strncasecmp($arg, 'false', 5)) {
+        $arguments[]= false;
+      } else if (0 === strncasecmp($arg, 'null', 4)) {
+        $arguments[]= null;
+      } else if (strstr($arg, '.')) {
+        $arguments[]= (double)$arg;
+      } else {
+        $arguments[]= (int)$arg;
+      }
+    }
+    return $arguments;
+  }
+
+  /**
+   * Resolves a name
+   *
+   * @param  string $namespace
+   * @param  string $name
+   * @return lang.XPClass
+   */
+  private function resolveType($namespace, $name) {
+    return XPClass::forName(strstr($name, '.') ? $name : $namespace.'.'.$name);
+  }
+
+  /**
+   * Parse implementation from a string.
+   *
+   * @param  string $namespace
+   * @param  string $input
+   * @return var
+   */
+  private function bindingTo($namespace, $input) {
+    if (false === ($p= strpos($input, '('))) {
+      return $this->resolveType($namespace, $input);
+    } else {
+      $class= $this->resolveType($namespace, substr($input, 0, $p));
+      if ($class->hasConstructor()) {
+        $arguments= $this->argumentsIn(substr($input, $p + 1, -1));
+        return $class->getConstructor()->newInstance($arguments);
+      } else {
+        return $class->newInstance();
+      }
+    }
+  }
+
+  /**
+   * Configures bindings on given injector
+   *
+   * @param  inject.Injector $injector
+   */
+  public function configure($injector) {
+    $namespace= $this->properties->getFirstSection();
+    do {
+      foreach ($this->properties->readSection($namespace) as $name => $implementation) {
+        $type= $this->resolveType($namespace, $name);
+        if (is_array($implementation)) {
+          foreach ($implementation as $name => $impl) {
+            $injector->bind($type, $this->bindingTo($namespace, $impl), $name);
+          }
+        } else {
+          $injector->bind($type, $this->bindingTo($namespace, $implementation));
+        }
+      }
+    } while ($namespace= $this->properties->getNextSection());
+  }
+}
---
+++ src/test/php/inject/unittest/ConfiguredBindingsTest.class.php
@@ -0,0 +1,88 @@
+<?php namespace inject\unittest;
+
+use inject\Injector;
+use inject\ConfiguredBindings;
+use util\Properties;
+use inject\unittest\fixture\Value;
+use inject\unittest\fixture\FileSystem;
+
+class ConfiguredBindingsTest extends \unittest\TestCase {
+
+  #[@test]
+  public function bind_class() {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      inject.unittest.fixture.Storage=inject.unittest.fixture.FileSystem
+    ')));
+    $this->assertEquals(new FileSystem(), $inject->get('inject.unittest.fixture.Storage'));
+  }
+
+  #[@test]
+  public function bind_instance() {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      inject.unittest.fixture.Storage=inject.unittest.fixture.FileSystem("/usr")
+    ')));
+    $this->assertEquals(new FileSystem('/usr'), $inject->get('inject.unittest.fixture.Storage'));
+  }
+
+  #[@test]
+  public function bind_named_class() {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      inject.unittest.fixture.Storage[files]=inject.unittest.fixture.FileSystem
+    ')));
+    $this->assertEquals(new FileSystem(), $inject->get('inject.unittest.fixture.Storage', 'files'));
+  }
+
+  #[@test]
+  public function bind_named_instance() {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      inject.unittest.fixture.Storage[files]=inject.unittest.fixture.FileSystem("/usr")
+    ')));
+    $this->assertEquals(new FileSystem('/usr'), $inject->get('inject.unittest.fixture.Storage', 'files'));
+  }
+
+  #[@test]
+  public function bind_multiple() {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      inject.unittest.fixture.Storage[user]=inject.unittest.fixture.FileSystem("~/.xp")
+      inject.unittest.fixture.Storage[system]=inject.unittest.fixture.FileSystem("/etc/xp")
+    ')));
+    $this->assertEquals(
+      [new FileSystem('~/.xp'), new FileSystem('/etc/xp')],
+      [$inject->get('inject.unittest.fixture.Storage', 'user'), $inject->get('inject.unittest.fixture.Storage', 'system')]
+    );
+  }
+
+  #[@test, @values([
+  #  ['null', null],
+  #  ['true', true],
+  #  ['false', false],
+  #  ['0', 0], ['-1', -1], ['1', 1],
+  #  ['0.0', 0.0], ['-1.5', -1.5], ['1.5', 1.5],
+  #  ['"test"', 'test'], ['"\"test\""', '"test"'],
+  #  ["'test'", 'test'], ["'\'test\''", "'test'"]
+  #])]
+  public function bind_instance_with($param, $expected) {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      inject.unittest.fixture.Value=inject.unittest.fixture.Value('.$param.')
+    ')));
+    $this->assertEquals(new Value($expected), $inject->get('inject.unittest.fixture.Value'));
+  }
+
+  #[@test, @values([
+  #  'Storage=FileSystem',
+  #  'Storage=FileSystem()',
+  #  'Storage=inject.unittest.fixture.FileSystem',
+  #  'Storage=inject.unittest.fixture.FileSystem()',
+  #  'inject.unittest.fixture.Storage=FileSystem',
+  #  'inject.unittest.fixture.Storage=FileSystem()',
+  #  'inject.unittest.fixture.Storage=inject.unittest.fixture.FileSystem',
+  #  'inject.unittest.fixture.Storage=inject.unittest.fixture.FileSystem()'
+  #])]
+  public function uses_propertyfile_sections_as_namespace($line) {
+    $inject= new Injector(new ConfiguredBindings(Properties::fromString('
+      [inject.unittest.fixture]
+      '.$line.'
+    ')));
+    $this->assertEquals(new FileSystem(), $inject->get('inject.unittest.fixture.Storage'));
+  }
+}
