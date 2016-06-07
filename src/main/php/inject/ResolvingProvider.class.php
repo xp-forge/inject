782161b614b6ebbe07370b522ba7ce977a8199c4
Timm Friebe <thekid@thekid.de>
2016-06-07 16:54:20 +0200

    Implement ArrayType::provider()

---
+++ src/main/php/inject/ResolvingProvider.class.php
@@ -0,0 +1,20 @@
+<?php namespace inject;
+
+#[@generic(implements= ['var'])]
+class ResolvingProvider extends \lang\Object implements Provider {
+  private $binding, $injector;
+
+  /**
+   * Creates a new provider which resolves a binding
+   *
+   * @param  inject.Binding $binding
+   * @param  inject.Injector $injector
+   */
+  public function __construct($binding, $injector) {
+    $this->binding= $binding;
+    $this->injector= $injector;
+  }
+
+  /** @return var */
+  public function get() { return $this->binding->resolve($this->injector); }
+}
